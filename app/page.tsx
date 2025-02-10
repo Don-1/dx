5da82c6761b0c5d6f804f0d8f020aaa2225e9d6f
don-1 <don-saynomore@hotmail.com>
2025-02-10 19:48:11 +0000

    Add main slide show

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,37 @@
+"use client";
+
+import { Autoplay, EffectFade, Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Swiper, SwiperSlide } from "swiper/react";
+
 export default function Home() {
   return (
-    <>
-      <div className='container mx-auto px-4'>
-        <h1>Hello world</h1>
-      </div>
-    </>
+    <main className="h-screen w-screen">
+      <Swiper
+        modules={[Autoplay, EffectFade, Navigation, Pagination]}
+        effect="fade"
+        speed={1000}
+        autoplay={{ delay: 9000, disableOnInteraction: false }}
+        className="h-full"
+      >
+        {/* Slide 1 */}
+        <SwiperSlide>
+          <div
+            className="h-full w-full bg-cover bg-center"
+            style={{ backgroundImage: "url('/images/slide1.jpg')" }}
+          ></div>
+        </SwiperSlide>
+        {/* Slide 2 */}
+        <SwiperSlide>
+          <div
+            className="h-full w-full bg-cover bg-center"
+            style={{ backgroundImage: "url('/images/slide2.jpg')" }}
+          ></div>
+        </SwiperSlide>
+      </Swiper>
+    </main>
   );
 }
