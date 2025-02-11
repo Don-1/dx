ef055ea4709cfd1f86dcfdd41c82d0aefc7279af
don-1 <don-saynomore@hotmail.com>
2025-02-11 08:29:01 +0000

    Make footer transparaent and disable NavItems for now

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -1,15 +1,29 @@
-import { NavItems } from '../navigation/navitems';
+"use client";
+
+import { usePathname } from "next/navigation";
+import { NavItems } from "../navigation/navitems";
 
 const Footer = () => {
+  const pathName = usePathname();
+  const isHome = pathName === "/";
   return (
-    <div className='w-full h-20 bg-sky-200 sticky bottom-0'>
-      <div className='container mx-auto px-4 h-full'>
-        <div className='flex justify-center items-center h-full'>
-          <ul className='hidden md:flex gap-x-6 text-white'>
+    <div
+      className={` ${
+        isHome ? "bg-transparent" : "bg-sky-200"
+      } py-4 text-center text-white fixed bottom-0 w-full z-50`}
+    >
+      {/* <div className="container mx-auto px-4 h-full"> */}
+      {/* <div className="flex justify-center items-center h-full">
+          <ul className="hidden md:flex gap-x-6 text-white">
             <NavItems />
           </ul>
-        </div>
+        </div> */}
+      <div className="container mx-auto">
+        <p>
+          &copy; {new Date().getFullYear()} My Portfolio. All rights reserved.
+        </p>
       </div>
+      {/* </div> */}
     </div>
   );
 };
