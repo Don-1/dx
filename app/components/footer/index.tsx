c487e96a1085e1a109853a33e436aa0171da26dc
don-1 <don-saynomore@hotmail.com>
2025-02-11 18:10:43 +0000

    Remove unsed import and commented code

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { usePathname } from 'next/navigation';
-import { NavItems } from '../navigation/navitems';
 
 const Footer = () => {
   const pathName = usePathname();
@@ -12,12 +11,6 @@ const Footer = () => {
         isHome ? 'bg-transparent' : 'bg-sky-200'
       } py-4 text-center text-white fixed bottom-0 w-full z-50`}
     >
-      {/* <div className="container mx-auto px-4 h-full"> */}
-      {/* <div className="flex justify-center items-center h-full">
-          <ul className="hidden md:flex gap-x-6 text-white">
-            <NavItems />
-          </ul>
-        </div> */}
       <div className="container mx-auto">
         <p>
           &copy; {new Date().getFullYear()} My Portfolio. All rights reserved.
