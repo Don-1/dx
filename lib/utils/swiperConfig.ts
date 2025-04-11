aa6e38a6ea6fb6299a28c217fc9e73d627237b71
don-1 <don-saynomore@hotmail.com>
2025-04-11 07:11:23 +0000

    Add fade effect

--- lib/utils/swiperConfig.ts
+++ lib/utils/swiperConfig.ts
@@ -2,6 +2,7 @@ import { Autoplay, EffectFade, Navigation, Pagination } from 'swiper/modules';
 
 export const defaultSwiperConfig = {
   modules: [Autoplay, EffectFade, Navigation, Pagination],
+  effect: "fade",
   speed: 1000,
   autoplay: {
     delay: 9000,
